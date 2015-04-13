127e1bdd12cac24255d1117d05415e5c09799bac
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-13 10:41:33 +0200

    remove kernel and dtb blobs
    kernel and dtbs are moved to vendor/sony/kernel
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_d2303.mk
+++ aosp_d2303.mk
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 TARGET_KERNEL_CONFIG := aosp_yukon_eagle_defconfig
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
 
 # Include others
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
