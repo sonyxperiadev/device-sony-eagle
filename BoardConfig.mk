1f88d29a49dade4d1bb3480508c5c9d83c2b66eb
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-07 20:44:58 +0100

    fix platform naming
    in initial commit platform was named Board and device which
    is confusing. This commit fixes the naming making things easy
    to understand

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/yukon/BoardConfig.mk
+include device/sony/yukon/PlatformConfig.mk
 
 TARGET_RECOVERY_FSTAB = device/sony/eagle/rootdir/fstab.yukon
 
